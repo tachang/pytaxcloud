aec912a15982ad1eeed4dc2c1aa2cbeee3ae380a
Jeff Tchang <Jeff.Tchang@gmail.com>
2013-01-12 23:24:33 +0000

    Added a get_rate method

--- pytaxcloud/taxcloud.py
+++ pytaxcloud/taxcloud.py
@@ -1,6 +1,10 @@
 from suds.client import Client
 from decimal import *
 
+
+class TaxCloudException(Exception):
+  pass
+
 class TaxCloud:
 
   def __init__(self, api_login_id, api_key):
@@ -9,48 +13,44 @@ class TaxCloud:
     self.soap_url = 'https://api.taxcloud.net/1.0/TaxCloud.asmx'
     self.wsdl_url = 'https://api.taxcloud.net/1.0/?wsdl' 
 
-    self.client = Client(url=wsdl_url, location=soap_url)
+    self.client = Client(url=self.wsdl_url, location=self.soap_url)
 
+  def convertToAddress(self, data):
 
-  def lookup():
-    pass
+    address = self.client.factory.create('Address')
+    address.Address1 = data['address1']
+    address.Address2 = data['address2']
+    address.City = data['city']
+    address.State = data['state']
+    address.Zip5 = data['zipcode']
+    return address
 
+  def get_rate(self, city, state, postal_code):
+    address = self.client.factory.create('Address')
+    address.Address1 = ""
+    address.Address2 = ""
+    address.City = city
+    address.State = state
+    address.Zip5 = postal_code
 
+    cart_items = self.client.factory.create('ArrayOfCartItem')
+    cart_item = self.client.factory.create('CartItem')
+    cart_item.Index = 1
+    cart_item.ItemID = 1
+    cart_item.TIC = 0
+    cart_item.Price = Decimal("1.00")
+    cart_item.Qty = 1
+    cart_items.CartItem = [cart_item]
 
-address = client.factory.create('Address')
-address.Address1 = "150 Dolores St"
-address.Address2 = ""
-address.City = "San Francisco"
-address.State = "CA"
-address.Zip5 = "94103"
+    response = self.client.service.Lookup(self.api_login_id, self.api_key, "NoCustomerID", "NoCartID",
+                                        cart_items, address, address, True, None)
 
-cart_items = client.factory.create('ArrayOfCartItem')
-print cart_items
+    if( response.ResponseType == 'Error' ):
+      raise TaxCloudException(response.Messages[0][0].Message)
 
+    return Decimal(str(response.CartItemsResponse[0][0].TaxAmount))
 
-cart_item = client.factory.create('CartItem')
-
-print cart_item
-
-cart_item.Index = 1
-cart_item.ItemID = 1
-cart_item.TIC = 0
-cart_item.Price = Decimal("100.00")
-cart_item.Qty = 1
-
-cart_items.CartItem = [cart_item]
-
-
-print client.service.Lookup(api_login_id,
-                            api_key,
-                            "customerid",
-                            "cartid",
-                            cart_items,
-                            address,
-                            address,
-                            False,
-                            None)
-"""
+    
 
 """
 Lookup(xs:string apiLoginID, xs:string apiKey, xs:string customerID, xs:string cartID, ArrayOfCartItem cartItems,
